fba4193b5f33bb6d2cd416aedbf2008a26477c8c
Arjun Saini <arjuns1026@gmail.com>
2023-11-16 20:14:44 -0500

    MS3 Partial Completion
    - Need to still fix and refactor the constructor
    - Other methods need to be completed

--- LineManager.cpp
+++ LineManager.cpp
@@ -1,8 +1,76 @@
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 #include "LineManager.h"
+#include "Utilities.h"
 
 namespace sdds {
     LineManager::LineManager(const std::string& file, const std::vector<Workstation*>& stations) {
+        std::ifstream f(file);
+
+        try {
+
+
+            if (!f) {
+                std::invalid_argument("File not open, try again");
+            } else {
+                // First we want to create a vector of records, so we can iterate over each one using algos or ranged based loops
+                std::string line{};
+                std::vector<std::string> records{};
+
+                // Variables for utilities class
+                Utilities U{};
+                size_t pos = 0;
+                bool more = true;
+                Utilities::setDelimiter('|');
+
+                while (!f.eof()) {
+                    std::getline(f, line);
+                    records.push_back(line);
+                }
+
+                for (auto &record: records) {
+                    more = true;
+                    bool nextExists = false;
+
+                    std::string activeStationString = U.extractToken(record, pos, more), nextStationString{};
+
+                    if (more) {
+                        nextStationString = U.extractToken(record, pos, more);
+                        nextExists = true;
+                    };
+
+                    // Find both of the stations in the vector provided to us
+                    auto active = std::find_if(stations.begin(), stations.end(), [=](const Workstation *station) {
+                        return activeStationString == station->getItemName();
+                    });
+
+                    auto next = std::find_if(stations.begin(), stations.end(), [=](const Workstation *station) {
+                        return nextStationString == station->getItemName();
+                    });
+
+                    // Once we find both of the stations, we can push them on to the active line
+                    m_activeLine.push_back(*active);
+
+                    // If there is a next station, we update that
+                    if (nextExists)
+                        (*active)->setNextStation(*next);
+                    else
+                        (*active)->setNextStation(nullptr);
+                }
+
+                // Find the first station
+                m_firstStation = m_activeLine.front();
+
+                // Update the size
+                m_cntCustomerOrder = g_pending.size();
+
+
+            }
+        } catch (...){
+            throw std::string("An error occured in the LineManager Constructor...");
+        }
+
 
     };
 
@@ -15,6 +83,8 @@ namespace sdds {
     };
 
     void LineManager::display(std::ostream& os) const{
-
+        for (auto& station : m_activeLine){
+            station->display(os);
+        };
     };
 }
--- Workstation.cpp
+++ Workstation.cpp
@@ -20,21 +20,65 @@ namespace sdds {
 
     bool Workstation::attemptToMoveOrder(){
 
+        bool moved = false;
+
+        // First check if the order has been filled and the inventory is = 0
+        // If the order is filled (no more service required) OR insufficient quantity, move to the next station
+        if (m_orders.front().isItemFilled(getItemName()) || getQuantity() == 0) {
+
+            // Check if the next station is not NULLPTR
+            if (getNextStation() != nullptr) {
+                *getNextStation() += std::move(m_orders.front());
+            } else {
+
+                /*
+                 * If there is no next station, we need to check if the front orders meets the requirements of completed
+                 * or incompleted
+                 */
+
+                // If the order is filled, move it to g_completed
+                if (m_orders.front().isOrderFilled()){
+                    // Since g_completed is a customer order type, there is not += overload
+                    g_completed.push_back(std::move(m_orders.front()));
+                } else {
+                    g_incomplete.push_back(std::move(m_orders.front()));
+                }
+            }
+
+            // We have to remove the element in the front because its no longer apart of this station since its filled.
+            m_orders.pop_front();
+            moved = true;
+        };
+
+        return moved;
     };
 
     void Workstation::setNextStation(Workstation* station){
-
+        if (station != nullptr){
+            m_pNextStation = station;
+        }
     };
 
     Workstation* Workstation::getNextStation() const{
         return m_pNextStation;
     };
 
-    void Workstation::display(std::ostream& os) const{
+    void Workstation::display(std::ostream& os) const {
+        if (os){
+            os << getItemName() << "-->";
+            if (getNextStation() != nullptr){
+                os << m_pNextStation->getItemName();
+            } else {
+                os << "End of Line";
+            }
+            os << std::endl;
+        }
+
 
     };
 
     Workstation& Workstation::operator+=(CustomerOrder&& newOrder){
+        m_orders.push_back(std::move(newOrder));
         return *this;
     };
 }
